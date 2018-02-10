81d46afc3d445688d4e084141e36e5408ef18c8d
zy <525153575@qq.com>
2018-02-10 21:38:02 +0800

    add Blueprint

--- manage.py
+++ manage.py
@@ -1,7 +1,9 @@
 # manage.py
+import unittest
 from flask_script import Manager
-from project import app, db
+from project import create_app, db
 
+app = create_app()
 manager = Manager(app)
 
 @manager.command
@@ -10,6 +12,15 @@ def recreate_db():
     db.drop_all()
     db.create_all()
     db.session.commit()
+
+@manager.command
+def test():
+    """运行测试."""
+    tests = unittest.TestLoader().discover('project/tests', pattern='test_*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
 
 if __name__ == '__main__':
     manager.run()
