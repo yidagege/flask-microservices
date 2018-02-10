95b9c02e609ea85c9142034cec3c361a950bbfd9
zy <525153575@qq.com>
2018-02-10 23:08:18 +0800

    adduser

--- project/api/views.py
+++ project/api/views.py
@@ -1,5 +1,8 @@
 # project/api/views.py
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request, render_template
+from sqlalchemy import exc
+from project import db
+from project.api.models import User
 
 users_blueprint = Blueprint('users', __name__)
 
@@ -9,3 +12,17 @@ def ping_pong():
         'status': 'success',
         'message': 'pong!'
     })
+
+@users_blueprint.route('/users', methods=['POST'])
+def add_user():
+    # 获取POST的数据
+    post_data = request.get_json()
+    email = post_data.get('email')
+    user = User(username=post_data.get('username'), email=email)
+    db.session.add(user)
+    db.session.commit()
+    response_data = {
+        'status': 'success',
+        'message': '%s was added!' % email
+    }
+    return jsonify(response_data), 201
--- project/tests/test_users.py
+++ project/tests/test_users.py
@@ -2,10 +2,23 @@ import json
 from project.tests.base import BaseTestCase
 
 class TestUserService(BaseTestCase):
-    def test_users(self):
-        """确保ping的服务正常."""
-        response = self.client.get('/ping')
-        data = json.loads(response.data.decode())
-        self.assertEqual(response.status_code, 200)
-        self.assertIn('pong', data['message'])
-        self.assertIn('success', data['status'])
+	def test_users(self):
+		"""确保ping的服务正常."""
+		response = self.client.get('/ping')
+		data = json.loads(response.data.decode())
+		self.assertEqual(response.status_code, 200)
+		self.assertIn('pong', data['message'])
+		self.assertIn('success', data['status'])
+
+	def test_add_user(self):
+		"""确保能够正确添加一个用户的用户到数据库中"""
+		with self.client:
+			response = self.client.post(
+	            '/users',
+	            data=json.dumps(dict(username='cnych', email='qikqiak@gmail.com')),
+	            content_type='application/json',
+	        )
+			data = json.loads(response.data.decode())
+			self.assertEqual(response.status_code, 201)
+			self.assertIn('qikqiak@gmail.com was added', data['message'])
+			self.assertEqual('success', data['status'])
