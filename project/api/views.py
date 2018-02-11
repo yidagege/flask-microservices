cde36bcf9d22d3471442caf43d0daa7b48dd8205
ChaneyZhang <ChaneyZhang@qiyi.com>
2018-02-11 15:40:17 +0800

    dev:健壮处理

--- project/api/views.py
+++ project/api/views.py
@@ -3,6 +3,7 @@ from flask import Blueprint, jsonify, request, render_template
 from sqlalchemy import exc
 from project import db
 from project.api.models import User
+from sqlalchemy import exc
 
 users_blueprint = Blueprint('users', __name__)
 
@@ -17,12 +18,66 @@ def ping_pong():
 def add_user():
     # 获取POST的数据
     post_data = request.get_json()
+    if not post_data:
+        response_data = {
+            'status': 'fail',
+            'message': 'Invalid payload.'
+        }
+        return jsonify(response_data), 400
     email = post_data.get('email')
-    user = User(username=post_data.get('username'), email=email)
-    db.session.add(user)
-    db.session.commit()
-    response_data = {
-        'status': 'success',
-        'message': '%s was added!' % email
+    username = post_data.get('username')
+    try:
+        user = User.query.filter_by(email=email).first()
+        if not user:
+            # 证明数据库中不存在该email的用户，可以添加
+            db.session.add(User(username=username, email=email))
+            db.session.commit()
+            response_data = {
+                'status': 'success',
+                'message': '%s was added!' % email
+            }
+            return jsonify(response_data), 201
+        # 证明该email已经存在
+        response_data = {
+            'status': 'fail',
+            'message': 'Sorry. That email already exists.'
+        }
+        return jsonify(response_data), 400
+    except exc.IntegrityError as e:
+        db.session.rollback()  # 出现异常了，回滚
+        response_data = {
+            'status': 'fail',
+            'message': 'Invalid payload.'
+        }
+        return jsonify(response_data), 400
+
+@users_blueprint.route('/users/<user_id>', methods=['GET'])
+def get_user(user_id):
+    """获取某用户的详细信息"""
+    response_object = {
+        'status': 'fail',
+        'message': 'User does not exist'
     }
-    return jsonify(response_data), 201
+    code = 404
+    try:
+        user = User.query.filter_by(id=int(user_id)).first()
+        if user:
+            response_object = {
+                'status': 'success',
+                'data': {
+                    'username': user.username,
+                    'email': user.email,
+                    'created_at': user.created_at
+                }
+            }
+            code = 200
+    except ValueError:
+        response_object = {
+            'status': 'fail',
+            'message': 'Param id error'
+        }
+        code = 400
+    finally:
+        return jsonify(response_object), code
+
+        
--- project/tests/test_users.py
+++ project/tests/test_users.py
@@ -1,5 +1,7 @@
 import json
 from project.tests.base import BaseTestCase
+from project.api.models import User
+from project import db
 
 class TestUserService(BaseTestCase):
 	def test_users(self):
@@ -22,3 +24,95 @@ class TestUserService(BaseTestCase):
 			self.assertEqual(response.status_code, 201)
 			self.assertIn('qikqiak@gmail.com was added', data['message'])
 			self.assertEqual('success', data['status'])
+
+	def test_add_user_invalid_json(self):
+	    """如果JSON对象为空，确保抛出一个错误。"""
+	    with self.client:
+	        response = self.client.post(
+	            '/users',
+	            data=json.dumps(dict()),
+	            content_type='application/json'
+	        )
+	        data = json.loads(response.data.decode())
+	        self.assertEqual(response.status_code, 400)
+	        self.assertIn('Invalid payload', data['message'])
+	        self.assertEqual('fail', data['status'])
+
+	def test_add_user_invalid_json_keys(self):
+	    """如果JSON对象中没有username或email，确保抛出一个错误。"""
+	    with self.client:
+	        response = self.client.post(
+	            '/users',
+	            data=json.dumps(dict(email='qikqiak@gmail.com')),
+	            content_type='application/json'
+	        )
+	        data = json.loads(response.data.decode())
+	        self.assertEqual(response.status_code, 400)
+	        self.assertIn('Invalid payload', data['message'])
+	        self.assertEqual('fail', data['status'])
+
+	    with self.client:
+	        response = self.client.post(
+	            '/users',
+	            data=json.dumps(dict(username='cnych')),
+	            content_type='application/json'
+	        )
+	        data = json.loads(response.data.decode())
+	        self.assertEqual(response.status_code, 400)
+	        self.assertIn('Invalid payload', data['message'])
+	        self.assertEqual('fail', data['status'])
+
+	def test_add_user_duplicate_user(self):
+	    """如果邮件已经存在确保抛出一个错误。"""
+	    with self.client:
+	        self.client.post(
+	            '/users',
+	            data=json.dumps(dict(
+	                username='cnych',
+	                email='qikqiak@gmail.com'
+	            )),
+	            content_type='application/json'
+	        )
+	        response = self.client.post(
+	            '/users',
+	            data=json.dumps(dict(
+	                username='cnych',
+	                email='qikqiak@gmail.com'
+	            )),
+	            content_type='application/json'
+	        )
+	        data = json.loads(response.data.decode())
+	        self.assertEqual(response.status_code, 400)
+	        self.assertIn('Sorry. That email already exists.', data['message'])
+	        self.assertEqual('fail', data['status'])
+
+	def test_get_user(self):
+	    user = User(username='cnych', email='qikqiak@gmail.com')
+	    db.session.add(user)
+	    db.session.commit()
+	    with self.client:
+	        response = self.client.get('/users/%d' % user.id)
+	        data = json.loads(response.data.decode())
+	        self.assertEqual(response.status_code, 200)
+	        self.assertTrue('created_at' in data['data'])
+	        self.assertEqual('cnych', data['data']['username'])
+	        self.assertEqual('qikqiak@gmail.com', data['data']['email'])
+	        self.assertEqual('success', data['status'])
+
+	def test_get_user_no_id(self):
+	    """如果没有id的时候抛出异常。"""
+	    with self.client:
+	        response = self.client.get('/users/xxx')
+	        data = json.loads(response.data.decode())
+	        self.assertEqual(response.status_code, 400)
+	        self.assertIn('Param id error', data['message'])
+	        self.assertEqual('fail', data['status'])
+
+	def test_get_user_incorrect_id(self):
+	    """如果ID不存在则要抛出异常"""
+	    with self.client:
+	        response = self.client.get('/users/-1')
+	        data = json.loads(response.data.decode())
+	        self.assertEqual(response.status_code, 404)
+	        self.assertIn('User does not exist', data['message'])
+	        self.assertEqual('fail', data['status'])
